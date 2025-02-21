6fb28166a9134889e76319564f02a32520286a4c
JungEun <foreverkiddd@gmail.com>
2025-02-21 12:43:55 +0900

    250221 라우팅 등

--- routes/channels.js
+++ routes/channels.js
@@ -1,29 +1,37 @@
 // express 모듈 셋팅
 const express = require('express')
-const app = express()
-app.listen(7777)
-app.use(express.json())   // http 외 모듈 'json'
+const router = express.Router()
+
+router.use(express.json())   // http 외 모듈 'json'
 
 let db = new Map()
 var id = 1  // 하나의 객체를 유니크하게 구별하기 위함
 
 // 채널 개별 생성
-app
-    .route('/channels')
+router
+    .route('/')
     // 채널 전체 조회
     .get((req, res) => {
-        if (db.size) {
-            var channels = []   // 대괄호로 변경
+        var {userId} = req.body
+        var channels = []   // 대괄호로 변경
 
-            db.forEach((value, key) => {
-                channels.push(value)
-            })
-
-            res.status(200).json(channels)
+        // 예외 처리 2가지
+        // 1) userId가 body에 없으면
+        if (db.size && userId) { // db가 비어있지 않고 userId가 있을 때
+                db.forEach((value, key) => {
+                    if (value.userId == userId) {
+                        channels.push(value)
+                    }
+                })
+    
+                // 2) userId가 가진 채널이 없으면
+                if (channels.length) {  // channels의 값이 있으면
+                    res.status(200).json(channels)
+                } else {
+                    notFoundChannel()
+                }
         } else {
-            res.status(404).json({
-                message : `조회할 채널이 없습니다.`
-            })
+            notFoundChannel()
         }
 
         // console.log(channels[0])
@@ -36,7 +44,8 @@ app
 
         // channelTitle 값이 있으면
         if(req.body.channelTitle) {
-            db.set(id++, req.body)
+            let channel = req.body
+            db.set(id++, channel)
 
             res.status(201).json({
                 message : `${db.get(id-1).channelTitle} 채널을 응원합니다!`
@@ -48,8 +57,8 @@ app
         }
     })
 
-app
-    .route('/channels/:id')
+router
+    .route('/:id')
     // 채널 개별 조회
     .get((req, res) => {
         let {id} = req.params
@@ -59,9 +68,7 @@ app
         if (channel) {
             res.status(200).json(channel)
         } else {
-            res.status(404).json({
-                message : `채널 정보를 찾을 수 없습니다.`
-            })
+            notFoundChannel()
         }
     })
     // 채널 개별 수정
@@ -81,9 +88,7 @@ app
                 message : `채널명이 정상적으로 수정되었습니다. 기존 : ${oldTitle} -> 수정 : ${newTitle}`
             })
         } else {
-            res.status(404).json({
-                message : `채널 정보를 찾을 수 없습니다.`
-            })
+            notFoundChannel()
         }
     })
     // 채널 개별 삭제
@@ -98,8 +103,14 @@ app
                 message : `${channel.channelTitle} 채널이 정상적으로 삭제되었습니다.`
             })
         } else {
-            res.status(404).json({
-                message : `채널 정보를 찾을 수 없습니다.`
-            })
+            notFoundChannel()
         }
     })
+
+function notFoundChannel() {
+    res.status(404).json({
+        message : `채널 정보를 찾을 수 없습니다.`
+    })
+}
+
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,14 @@
 // express 모듈 셋팅
 const express = require('express')
-const app = express()
-app.listen(7777)
+const router = express.Router()
 
 let db = new Map()
 var id = 1  // 하나의 객체를 유니크하게 구별하기 위함
 
-app.use(express.json())   // http 외 모듈 'json'
+router.use(express.json())   // http 외 모듈 'json' 미들웨어 생성
 
 // 로그인
-app.post('/login', (req, res) => {
+router.post('/login', (req, res) => {
     console.log(req.body)   // userId, pwd
 
     // userId가 DB에 저장된 회원인지 확인
@@ -17,15 +16,22 @@ app.post('/login', (req, res) => {
     var loginUser = search(userId)
 
     if (isExist(loginUser)) { // length === 1 일 것임
-        console.log(`같은 거 찾았다!`)
+        // console.log(`같은 거 찾았다!`)
         // pwd도 맞는지 비교 (자료형도 같아야 함)
         if (loginUser.pwd === pwd) {
-            console.log(`패스워드 같습니다!`)
+            // console.log(`패스워드 같습니다!`)
+            res.status(200).json({
+                message : `${loginUser.name}님 로그인 되었습니다.`
+            })
         } else {
-            console.log(`패스워드 틀렸습니다!`)
+            res.status(400).json({
+                message : `비밀번호가 틀렸습니다.`
+            })
         }
     } else {
-        console.log(`입력하신 ID는 없는 ID입니다.`)
+        res.status(404).json({
+            message : `회원 정보가 없습니다.`
+        })
     }
 })
 
@@ -52,7 +58,7 @@ function isExist(obj) {
 }
 
 // 회원 가입
-app.post('/join', (req, res) => {
+router.post('/join', (req, res) => {
     console.log(req.body)
 
     if (req.body == {}) {
@@ -60,54 +66,57 @@ app.post('/join', (req, res) => {
             message : `입력 값을 다시 확인해주세요.`
         })
     } else {
-        db.set(id++, req.body)
+        const {userId} = req.body
+                // 이제 userId로 키값을 받음
+        db.set(userId, req.body)
 
         // 상태코드는 201(등록 성공)
         res.status(201).json({
             // id값이 ++되었으므로 -1 해주기
-            message : `${db.get(id-1).name}님 회원 가입해주셔서 감사합니다!`
+            message : `${db.get(userId).name}님 회원 가입해주셔서 감사합니다!`
         })
     }
 })
 
-app
-    .route('/users/:id')
+router
+    .route('/users')
     
     // 회원 개별 조회
     .get((req, res) => {
-        let {id} = req.params
-        id = parseInt(id)
+        let {userId} = req.body
+        // id = parseInt(id) // userId 문자열로 받으니 이제 필요 없음
 
         // console.log(id)
 
-        const user = db.get(id)
-        if (user == undefined) {
-            res.status(404).json({
-                message : `회원 정보가 없습니다.`
-            })
-        } else {
+        const user = db.get(userId)
+        if (user) {
             res.status(200).json({
                 userId : user.userId,
                 name : user.name
+            })
+        } else {
+            res.status(404).json({
+                message : `회원 정보가 없습니다.`
             })
         }
     })
 
     // 회원 개별 탈퇴
     .delete((req, res) => {
-        let {id} = req.params
-        id = parseInt(id)
-    
-        const user = db.get(id)
-        if (user == undefined) {
-            res.status(404).json({
-                message : `회원 정보가 없습니다.`
-            })
-        } else {
-            db.delete(id)
+        let {userId} = req.body
+
+        const user = db.get(userId)
+        if (user) {
+            db.delete(userId)
     
             res.status(200).json({
                 message : `${user.name}님 탈퇴가 완료되었습니다.`
             })
+        } else {
+            res.status(404).json({
+                message : `회원 정보가 없습니다.`
+            })
         }
     })
+
+module.exports = router
